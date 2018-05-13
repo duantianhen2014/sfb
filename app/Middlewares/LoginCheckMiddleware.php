37bca8e2a6ff057b21c269050bb2cf1b12a23ba7
小滕 <616896861@qq.com>
2018-05-13 11:19:36 +0800

    完成用户登录

---
+++ app/Middlewares/LoginCheckMiddleware.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact group@swoft.org
+ * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace App\Middlewares;
+
+use App\Foundations\Auth\AuthSign;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Swoft\Bean\Annotation\Bean;
+use Swoft\Http\Message\Middleware\MiddlewareInterface;
+use Swoft\Bean\Annotation\Inject;
+use Swoft\Log\Log;
+
+/**
+ * Class LoginCheckMiddlewareMiddleware - Custom middleware
+ * @Bean()
+ * @package App\Middlewares
+ */
+class LoginCheckMiddleware implements MiddlewareInterface
+{
+
+    protected $white = [
+        'login',
+    ];
+
+    /**
+     * @Inject(name="auth.sign")
+     * @var AuthSign
+     */
+    protected $authSign;
+
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Server\RequestHandlerInterface $handler
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \InvalidArgumentException|\Exception
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $isNeedCheck = true;
+        $uri = $request->getUri()->getPath();
+        foreach ($this->white as $item) {
+            if (preg_match('#' . $item . '#', $uri)) {
+                $isNeedCheck = false;
+                break;
+            }
+        }
+
+        if ($isNeedCheck) {
+            $token = $request->getHeaderLine('AccessToken');
+            if (! $this->authSign->check($token)) {
+                throw new \Exception('请登录', 401);
+            }
+        }
+
+        return $handler->handle($request);
+    }
+}
